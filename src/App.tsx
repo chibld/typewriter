37c1f9b3a0bb6100eb69027594105a018601aa5d
chibld <archie.anderson@mail.com>
2026-03-09 00:16:33 +0000

    Create random words generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
+import { faker } from "@faker-js/faker";
+import RestartButton from "./components/RestartButton";
+import Results from "./components/Results";
+import UserTypings from "./components/UserTypings";
+
+const words = faker.word.words(10);
+
 function App() {
   return (
-    <div >
+    <>
+      <CountdownTimer timeLeft={30} />
+      <div className="relative max-w-xl mt-3 text-3xl leading-relaxed break-all">
+        <GeneratedWords words={words} />
+        <UserTypings className="absolute inset-0" userInput={words} />
+      </div>
 
-    </div>
+      <RestartButton
+        className={"mx-auto mt-10 text-slate-500"}
+        onRestart={() => null}
+      />
+      <Results className="mt-10" errors={10} accuracy={100} total={200} />
+    </>
   );
 }
+
+const GeneratedWords = ({ words }: { words: string }) => {
+  return <div className="text-slate-500">{words}</div>;
+};
+
+const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="text-primary-400 font-medium">Time: {timeLeft}</h2>;
+};
 
 export default App;
